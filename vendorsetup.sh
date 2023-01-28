1ff5ff9f5c91a5289d8006e0aeae2120976eb202
Galitsky Dmytro <melles.android@gmail.com>
2023-01-28 13:12:50 +0200

    official: vendorsetup: beta: Bump up FOX_VERSION to R11.1_1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -118,7 +118,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
 	# Redmi 7/Y3 specific
 	export FOX_BUILD_TYPE="Beta"
-	export FOX_VERSION=R11.1_0
+	export FOX_VERSION=R11.1_1
 	export OF_MAINTAINER=melles1991
 	export OF_USE_TWRP_SAR_DETECT=1
 
