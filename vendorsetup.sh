60b6966254dbcba71bcc529f155e58e728476742
Galitsky Dmytro <melles.android@gmail.com>
2023-03-09 20:05:29 +0200

    onclite: vendorsetup.sh: Minor corrections made.
    - Duplicates have been removed and unnecessary items have been deleted.
    
    Thank you for your attention, @DarthJabba9.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -34,32 +34,11 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
    fox_get_target_device
 fi
 
-fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
-   if [ -n "$chkdev" ]; then
-      FOX_BUILD_DEVICE="$FDEVICE"
-   else
-      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi
-}
-
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-   fox_get_target_device
-fi
-
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export LC_ALL="C"
 	export ALLOW_MISSING_DEPENDENCIES=true
 	export OF_TARGET_DEVICES=onclite
 	export TARGET_DEVICE_ALT=onclite,onc
-
-
-	# Device Tree Path | Include Path
-	export DEVICE_PATH=device/xiaomi/onclite
-	export INCLUDE_PATH=$DEVICE_PATH/include
-
-	export OF_MAINTAINER_AVATAR=$INCLUDE_PATH/melles1991.png
 
 	#Language
 	export TW_DEFAULT_LANGUAGE="en"
@@ -84,7 +63,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_USE_SYSTEM_FINGERPRINT=1
 
 	#Addons
-	export FOX_DELETE_INITD_ADDON=1 # !- Causes bootloops sometimes -!	
+	export FOX_DELETE_INITD_ADDON=1 # !- Causes bootloops sometimes -!
 	export FOX_DELETE_AROMAFM=1
 
 	# LZMA compression - this is needed!
@@ -129,5 +108,5 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
  	fi
-  	
+
 fi
